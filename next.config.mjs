93dbbb41099cf5419e5494309ca705c5f5470d26
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2024-10-09 16:53:52 +0200

    Remove redirects from next config and set output to export

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  redirects() {
-    return [
-      {
-        source: "/smdd2024",
-        has: [
-          {
-            type: "query",
-            key: "lang",
-            value: "(?<lang>.*)",
-          },
-        ],
-        destination: "/smdd2024/:lang",
-        permanent: true,
-      },
-      {
-        source: "/smdd2024",
-        destination: "/smdd2024/en",
-        permanent: true,
-      },
-    ];
-  },
+  output: "export",
 };
 
 export default nextConfig;
