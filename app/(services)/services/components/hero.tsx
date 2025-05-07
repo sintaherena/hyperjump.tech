7ab47ed74fa983833db269546cfa72ae2b5adb23
Hari Nugraha <15191978+haricnugraha@users.noreply.github.com>
2025-05-07 11:11:39 +0700

    Copy: Change Service Clients Copy (#26)

--- app/(services)/services/components/hero.tsx
+++ app/(services)/services/components/hero.tsx
@@ -10,10 +10,12 @@ export default function Hero({ lang }: { lang: SupportedLanguage }) {
   return (
     <section className="relative h-[415px] w-full bg-services-hero px-4 text-center text-hyperjump-black">
       <div className="mx-auto flex h-full max-w-3xl flex-col items-center justify-center pt-16">
-        <h1 className="mb-4 text-4xl font-medium text-hyperjump-black md:text-[40px]">
+        <h1 className="mb-4 text-2xl font-medium text-hyperjump-black sm:text-4xl md:text-[40px]">
           {servicesHeroHeading(lang)}
         </h1>
-        <p className="text-lg text-hyperjump-gray">{servicesHeroDesc(lang)}</p>
+        <p className="text-base text-hyperjump-gray sm:text-lg">
+          {servicesHeroDesc(lang)}
+        </p>
       </div>
     </section>
   );
