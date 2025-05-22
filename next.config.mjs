a35489defa02006f514ef2390ad1bb0c243b750a
sinta <sintaherena@gmail.com>
2025-05-22 11:46:34 +0700

    update to dynamically generated

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
 const basePath = ""; // process.env.NODE_ENV === "production" ? "/hyperjump.tech" : "";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  // output: "export",
   basePath,
   assetPrefix: basePath,
   images: {
     unoptimized: true
-  }
+  },
+  reactStrictMode: true
 };
 
 export default nextConfig;
