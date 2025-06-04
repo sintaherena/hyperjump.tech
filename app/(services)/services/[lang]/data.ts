3a6c5623e903ee767bc3fe200c0348bf875f7a0f
sinta <sintaherena@gmail.com>
2025-06-04 10:49:00 +0700

    Fix: Unrelated case studies are shown on the service detail page

--- app/(services)/services/[lang]/data.ts
+++ app/(services)/services/[lang]/data.ts
@@ -1,31 +1,9 @@
-import { SupportedLanguage } from "@/locales/.generated/types";
 import {
   servicesHeroDesc,
-  servicesHeroHeading,
-  caseStudyErpFisheriesTitle,
-  caseStudyErpFisheriesDesc,
-  caseStudyCtoaasMediaTitle,
-  caseStudyCtoaasMediaDesc
+  servicesHeroHeading
 } from "@/locales/.generated/server";
 
 export const data = {
   title: `Services - ${servicesHeroHeading("en")}`,
   description: servicesHeroDesc("en")
-};
-
-export const getCaseStudies = (lang: SupportedLanguage) => {
-  const data = [
-    {
-      title: caseStudyErpFisheriesTitle(lang),
-      description: caseStudyErpFisheriesDesc(lang),
-      url: "/case-studies/erp-fisheries"
-    },
-    {
-      title: caseStudyCtoaasMediaTitle(lang),
-      description: caseStudyCtoaasMediaDesc(lang),
-      url: "/case-studies/ctoaas-media"
-    }
-  ];
-
-  return data;
 };
